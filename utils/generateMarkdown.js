4c180921f87975225aa338888d6f9416562d3d09
eahagan <57505890+eahagan@users.noreply.github.com>
2025-02-05 00:08:18 -0500

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to generate a license badge based on user selection
+function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to generate a license link
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `\n- [License](#license)`;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to generate the license section of the README
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${license} license.`;
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+  `;
 }
 
-export default generateMarkdown;
+module.exports = generateMarkdown;
