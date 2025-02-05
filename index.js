fb264527b6b9450b3d7e13cc2429e76f6c60e8d9
eahagan <57505890+eahagan@users.noreply.github.com>
2025-02-05 00:06:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions for the user
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install this project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to this project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How do you test this project?',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write the README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('README.md successfully generated!');
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize the app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers);
+    writeToFile('README.md', markdownContent);
+  });
+}
 
-// Function call to initialize app
+// Start the application
 init();
